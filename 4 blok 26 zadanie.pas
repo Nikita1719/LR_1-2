61c534246efac3475839dfffa4466cca5900b185
Nikita1719 <144107423+Nikita1719@users.noreply.github.com>
2023-09-18 21:30:51 +0300

    Add files via upload

---
+++ 4 blok 26 zadanie.pas
@@ -0,0 +1,14 @@
+﻿program k3;
+var
+  a, b, c: integer;
+begin
+writeln('Введите числа: ');
+  readln(a);
+  readln(b);
+  readln(c);
+
+  if (a + b > c) and (a + c > b) and (b + c > a) then
+    writeln('Да')
+  else
+    writeln('Нет');
+end.
