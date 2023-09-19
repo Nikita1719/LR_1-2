5a5e92cbd028cc52ea5dd6e8e277964f34bf93fc
Nikita1719 <144107423+Nikita1719@users.noreply.github.com>
2023-09-19 12:39:10 +0300

    Add files via upload

---
+++ Baza 4 27 zadanie.pas
@@ -0,0 +1,10 @@
+﻿var a,b,c:real ; 
+begin 
+writeln ('Введите 3 стороны треугольника');
+readln (a,b,c); 
+if (a>b+c) or (b>a+c) or (c>a+b) then write ('impossible') 
+else if (a*a=b*b+c*c) or (b*b=a*a+c*c) or (c*c=a*a+b*b) then write ('rectangular') 
+else if (a*a>b*b+c*c) or (b*b>a*a+c*c) or (c*c>a*a+b*b) then write ('obtuse') 
+else write ('acute'); 
+readln; 
+end.
